b62d849cbef2354dfb477c28b9dd05ca496a7c7c
oldmansaturn <dalton.herriman76@icloud.com>
2025-03-06 17:02:36 -0600

    updates to navbar, templates, app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template('home.html')
 @app.route('/home')
-def home_page():
+def home():
     return render_template('home.html')
 
 @app.route('/contact')
@@ -18,11 +22,6 @@ def base_page():
 @app.route('/about')
 def about_page():
     return render_template('about.html')
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
