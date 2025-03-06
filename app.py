2cc93423567b3ac2b5967ade36acf5c64624ac95
oldmansaturn <dalton.herriman76@icloud.com>
2025-03-06 16:46:17 -0600

    added initial routes and templates

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask
+from flask import render_template
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+@app.route('/base')
+def base_page():
+    return render_template('base.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
 app = Flask(__name__)
 
 @app.route('/')
