59d978ba393a758db4671f57e7c56bb281829297
oldmansaturn <dalton.herriman76@icloud.com>
2025-03-06 16:48:45 -0600

    Updated route and template for homepage

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+
+app = Flask(__name__)
+
 @app.route('/home')
 def home_page():
     return render_template('home.html')
@@ -19,7 +22,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to ACME Labs Inc!"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
